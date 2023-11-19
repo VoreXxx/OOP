1bda60774bffa1cc0ee729c76faf4a4a40393532
VoreXxx <vorex.00@mail.ru>
2023-11-19 18:54:05 +0600

    fixed errors

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,50 @@
 public class Main {
     public static void main(String[] args) {
+            Grifindor harry = new Grifindor("Harry",90, 85,
+                    88, 67, 88);
+            Grifindor hermione = new Grifindor("Hermione", 85, 90,
+                    85, 94, 76);
+            Grifindor ron = new Grifindor("Ron", 88, 80,
+                    85, 65, 70);
+
+            Hufflepuff zacharias = new Hufflepuff("Zachrias", 88, 90,
+                    80, 30, 98);
+            Hufflepuff sedrick = new Hufflepuff("Sedrick",92, 83,
+                    95, 87, 65);
+            Hufflepuff justin = new Hufflepuff("Justin",90, 87,
+                    98,44, 55);
+
+            Ravenclaw cho = new Ravenclaw("Cho",95, 85,
+                    90, 45, 78);
+            Ravenclaw padma = new Ravenclaw("Padma",78, 80,
+                    85, 66, 32);
+            Ravenclaw marcus = new Ravenclaw("Marcus", 80, 65,
+                    79, 45,78);
+
+            Slytherin draco = new Slytherin("Draco",80, 90,
+                    95, 85, 90, 66, 99);
+            Slytherin grehem = new Slytherin("Grehem", 58, 79,
+                    65, 78, 30, 45, 21);
+            Slytherin gregory = new Slytherin("Gregory", 40, 51,
+                    67, 69, 63, 39, 87);
+
+        System.out.println(harry.toString());
+        System.out.println(hermione.toString());
+        System.out.println(ron.toString());
+        System.out.println(zacharias.toString());
+        System.out.println(sedrick.toString());
+        System.out.println(justin.toString());
+        System.out.println(cho.toString());
+        System.out.println(padma.toString());
+        System.out.println(marcus.toString());
+        System.out.println(draco.toString());
+        System.out.println(grehem.toString());
+        System.out.println(gregory.toString());
+
+        draco.compareSlytherinStudents(gregory);
+        ron.compareGrifindorStudents(harry);
+        harry.compareMagic(draco);
+        hermione.compareMagic(cho);
+
     }
 }
---
+++ src/Slytherin.java
@@ -0,0 +1,60 @@
+public class Slytherin extends Hogwarts{
+    private final int cunning;
+    private final int determination;
+    private final int ambition;
+    private final int resourcefulness;
+    private final int thirstPower;
+
+    public Slytherin(String name, int magicPower, int transgession, int cunning,
+                     int determination, int ambition, int resourcefulness, int thirstPower) {
+        super(name, magicPower, transgession);
+        this.cunning = cunning;
+        this.determination = determination;
+        this.ambition = ambition;
+        this.resourcefulness = resourcefulness;
+        this.thirstPower = thirstPower;
+    }
+
+    public int getCunning() {
+        return cunning;
+    }
+
+    public int getDetermination() {
+        return determination;
+    }
+
+    public int getAmbition() {
+        return ambition;
+    }
+
+    public int getResourcefulness() {
+        return resourcefulness;
+    }
+
+    public int getThirstPower() {
+        return thirstPower;
+    }
+
+    public void compareSlytherinStudents (Slytherin student){
+        int totalStudent1 = this.getCunning() + this.getDetermination() +
+                this.getAmbition() + this.getResourcefulness() + this.getThirstPower();
+        int totalStudent2 = student.getCunning() + student.getDetermination() +
+                student.getAmbition() + student.getResourcefulness() + student.getThirstPower();
+        if (totalStudent1 > totalStudent2) {
+            System.out.println(this.getName() + " " + totalStudent1 + " is a better student than "
+                    + student.getName() + " " + totalStudent2);
+        } else {
+            System.out.println(student.getName() + " " + totalStudent2 + " is a better student than "
+                    + this.getName() + " " + totalStudent1);
+        }
+    }
+    @Override
+    public String toString() {
+        return super.toString() +
+                ", cunning= " + cunning +
+                ", determination= " + determination +
+                ", ambition= " + ambition +
+                ", resourcefulness= " + resourcefulness +
+                ", thirstPower= " + thirstPower;
+    }
+}
